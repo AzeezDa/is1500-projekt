092f50d8f92a89b961b7cbbf6ea51734ce16c1a0
AzeezDaoud <azeez.daoud3@gmail.com>
2022-09-29 19:50:28 +0200

    Comment painter file

--- painter.c
+++ painter.c
@@ -3,17 +3,31 @@
 // Given a x, y coordinate, set that pixel ON on the board
 void pixon(const BYTE x, const BYTE y)
 {
-    BYTE bit_offset = y >> 3;
-    int buf_coord = (bit_offset << 7) + x;
-    bit_offset = y - (bit_offset << 3);
-    display_buffer[buf_coord] |= 1 << bit_offset;
+    // Determine which row the bit is (there are 8 bits in each row so we divide by 8)
+    BYTE row = y >> 3; 
+
+    // Multiply row by 128 (<< 7) to push it in the correct place Y position in the buffer (now at (0,Y)). Add X to push it to the correct column (X,Y)
+    int buf_coord = (row << 7) + x;
+
+    // We want to know which bit of that byte we want to modify, so we get the last three bits (0-7) which tell us the bit offset aka modulo 8
+    row = y & 7;
+
+    // We push a one with that many steps forward in that byte in the buffer and or it to make it 1 at the specific bit
+    display_buffer[buf_coord] |= 1 << row;
 }
 
 // Given a x, y coordinate, set that pixel OFF on the board
 void pixoff(const BYTE x, const BYTE y)
 {
-    BYTE bit_offset = y >> 3;
-    int buf_coord = (bit_offset << 7) + x;
-    bit_offset = y - (bit_offset << 3);
-    display_buffer[buf_coord] &= ~(1 << bit_offset);
+        // Determine which row the bit is (there are 8 bits in each row so we divide by 8)
+    BYTE row = y >> 3; 
+
+    // Multiply row by 128 (<< 7) to push it in the correct place Y position in the buffer (now at (0,Y)). Add X to push it to the correct column (X,Y)
+    int buf_coord = (row << 7) + x;
+
+    // We want to know which bit of that byte we want to modify, so we get the last three bits (0-7) which tell us the bit offset aka modulo 8
+    row = y & 7;
+
+    // We push a one with that many steps forward in that byte in the buffer and and it with the inverse of that byte to make it 0 at the specific bit
+    display_buffer[buf_coord] &= ~(1 << row);
 }
