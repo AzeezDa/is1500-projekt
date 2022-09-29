1401d62d8076c58df4384e25cf4e3a2c9500374b
AzeezDaoud <azeez.daoud3@gmail.com>
2022-09-29 22:31:14 +0200

    Fix minor chipKIT warnings and add pendulum

--- main.c
+++ main.c
@@ -6,11 +6,34 @@ int main()
 {
 	init();
 
-	pixon(1, 1);
-	pixon(6, 9);
-	pixon(4, 20);
+	float x = 16, t = 0;
+	int i, j;
+	
+	while (1)
+	{
+		// Draw pendulum rope
+		x = 64.0 + 20 * sin(t);
+		draw_line(64, 1, x, 25);
+		t += 0.01;
+		
+		// Pendulum ball
+		for (i = -3; i < 3; i++)
+		{
+			for (j = -3; j < 3; j++)
+				pixon(x + i, 25 + j);
+		}
+		
+		oled_put_buffer();
 
-	oled_put_buffer();
+		for (i = 0; i < 512; i++)
+		{
+			display_buffer[i] = 0;
+		}
+		
+	}
+	
+
+	
 
 	for (;;)
 		;
