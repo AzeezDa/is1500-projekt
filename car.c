cbe4647783ad4852f77079dc7c26945390200306
Herdi <herdisaleh02@gmail.com>
2022-12-09 17:35:25 +0100

    Added animations to steering car

--- car.c
+++ car.c
@@ -208,17 +208,18 @@ void update_player(const inputs i) // Inlineable?
     }
 
     // Sideways steering
-    if (i.b4) 
+    if(i.b3 || i.b4) 
     {
-        car.texture = &frame_car_l;
-        turn_car(-car.turn_speed * steer_modulation);
-    }
-    else if (i.b3) 
-    {
-        car.texture = &frame_car_r;
-        turn_car(car.turn_speed * steer_modulation);
-    }
-    else 
+        if (i.b4) {
+            car.texture = &frame_car_l;
+            turn_car(-car.turn_speed * steer_modulation);
+        }
+        if (i.b3) {
+            car.texture = &frame_car_r;
+            turn_car(car.turn_speed * steer_modulation);
+        }
+    } 
+    else
     {
         car.texture = &frame_car_fw;
     }
